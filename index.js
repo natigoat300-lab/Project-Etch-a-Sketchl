25a28933bb739cb8490f915849350d6a16871502
natigoat37 <natigoat300@gmail.com>
2026-05-04 12:17:25 +0300

    feat(grid): implement dynamic grid generation and flexbox layout

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const body = document.querySelector('body')
 const container = document.createElement('div')
 container.classList.add('container')
 body.append(container)
-// create divs
+// create a function that create divs
+let user =  parseInt(prompt("insert < 100"))
+function createDiv() {
+    for(let i = 0; i < user; i++) {
+        let div = document.createElement('div')
+        div.classList.add('square')
+        container.append(div)
+        for(let i = 0; i < user; i++) {
+            let divs = document.createElement('div')
+            div.append(divs)
+        }
+
+    }
+}
+createDiv()
+

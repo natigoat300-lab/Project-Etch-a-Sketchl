ff601a5793a2b77b5dfc1bbc717c85b60f3d19c9
natigoat37 <natigoat300@gmail.com>
2026-05-04 08:34:48 +0300

    feat(layout): create main grid container and center on page

---
+++ index.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body')
+//create a div container
+const container = document.createElement('div')
+container.classList.add('container')
+body.append(container)
+// create divs

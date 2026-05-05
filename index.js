df6dbd542350228040f79fa42d7a2694e4c366ea
natigoat37 <natigoat300@gmail.com>
2026-05-05 05:21:41 +0300

    (feat):limit grid size to 100 and add 16x16 fallback

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const container = document.createElement('div')
 container.classList.add('container')
 body.append(container)
 // create a function that create divs
-let user =  parseInt(prompt("insert < 100"))
-function createDiv() {
-    for(let i = 0; i < user; i++) {
+
+let user =  parseInt(prompt("Enter a number not more than 100 and not less than 1"))
+
+
+
+function createDiv(userInput) {
+    if (userInput <= 100 && userInput > 0) {
+        for(let i = 0; i < userInput; i++) {
         let div = document.createElement('div')
         div.classList.add('square')
         container.append(div)
-        for(let i = 0; i < user; i++) {
+        for(let i = 0; i < userInput; i++) {
             let divs = document.createElement('div')
             div.append(divs)
         }
 
     }
+    }else{
+        alert("Invalid value! will set it to 16x16")
+        createDiv(16)
+    }
 }
-createDiv()
+createDiv(user)
 

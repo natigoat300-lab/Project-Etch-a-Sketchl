00227ae41fe16df6029fbdb8abbcb6be90f3975a
natigoat37 <natigoat300@gmail.com>
2026-05-05 06:06:25 +0300

    feat: implement SET GRID button to handle user input

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const body = document.querySelector('body')
 const container = document.createElement('div')
 container.classList.add('container')
 body.append(container)
+//ask user input
+const humanChoice = function() {
+    let user =  parseInt(prompt("Enter a number not more than 100 and not less than 1"))
+    return user
+}
 // create a function that create divs
-
-let user =  parseInt(prompt("Enter a number not more than 100 and not less than 1"))
-
-
-
 function createDiv(userInput) {
+    container.innerHTML = '';
     if (userInput <= 100 && userInput > 0) {
         for(let i = 0; i < userInput; i++) {
         let div = document.createElement('div')
@@ -19,12 +20,16 @@ function createDiv(userInput) {
             let divs = document.createElement('div')
             div.append(divs)
         }
-
     }
     }else{
         alert("Invalid value! will set it to 16x16")
         createDiv(16)
     }
 }
-createDiv(user)
+createDiv(16)
+//add button that call the creatdiv function
+const btn = document.createElement('button')
+btn.textContent = "SET GRID"
+btn.addEventListener('click', () => createDiv(humanChoice()))
+body.append(btn)
 
